dc35cd61ee4f61a47a9ac50fb31e74f93230d445
Jorge Matos <jisapjisap@gmail.com>
2025-04-15 19:14:04 +0200

    Replace Button component with a paragraph element for a simpler greeting in the Home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
-      <Button>
-        Hello World
-      </Button>
+      <p className="text-rose-500">
+        Hello world
+      </p>
     </div>
   );
 }
