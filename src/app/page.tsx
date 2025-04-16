137a68325a30d92cc1db8ad699f153964a586f14
Jorge Matos <jisapjisap@gmail.com>
2025-04-16 18:51:38 +0200

    Update UI components and styles: modify button styles, enhance Home page layout with new components (Button, Input, Progress, Textarea, Checkbox), and adjust global CSS colors for border and input.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500">
-        Hello world
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">
+            Button
+          </Button>
+        </div>
+
+        <div>
+          <Input 
+            placeholder="input"
+          />
+        </div>
+
+        <div>
+          <Progress value={50} />
+        </div>
+
+        <div>
+          <Textarea placeholder="Textarea"/>
+        </div>
+
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
