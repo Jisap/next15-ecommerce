9e41fe8136d76125887c5b4f6ff6f5d4c71cdb0d
Jorge Matos <jisapjisap@gmail.com>
2025-04-19 14:55:00 +0200

    Add category dropdown with subcategory menu: Implement interactive dropdown for categories with hover functionality, including dynamic positioning for subcategories. Introduce useDropdownPosition hook for position calculations and create SubcategoryMenu component for rendering subcategories.

--- src/app/(app)/(home)/search-filter/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filter/category-dropdown.tsx
@@ -1,6 +1,11 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Category } from "@/payload-types"
+import { useRef, useState } from "react";
+import UseDropdownPosition from "./use-dropdown-position";
+import SubcategoryMenu from "./subcategory-menu";
 
 interface Props {
   category: Category;
@@ -9,15 +14,56 @@ interface Props {
 }
 
 const CategoryDropdown = ({ category, isActive, isNavigatioHovered }: Props) => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const { getDropdownPosition } = UseDropdownPosition(dropdownRef);
+
+  const onMouseEnter = () => {
+    if(category.subcategories){
+      setIsOpen(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    setIsOpen(false);
+  };
+
+  const dropdownPosition = getDropdownPosition();
+
   return (
-    <Button 
-      variant="elevated"
-      className={cn(
-        "h-11 px-4 bg-transparent rounded-full hover:bg-white hover:border-primary text-black"
-      )}
+    <div 
+      className="relative"
+      ref={dropdownRef}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
-      {category.name}
-    </Button>
+      <div className="relative">
+        <Button 
+          variant="elevated"
+          className={cn(
+            "h-11 px-4 bg-transparent rounded-full hover:bg-white hover:border-primary text-black",
+            isActive && !isNavigatioHovered && "bg-white border-primary",
+          )}
+        >
+          {category.name}
+        </Button>
+
+        {category.subcategories && category.subcategories.length > 0 && (
+          <div className={cn(
+            "opacity-0 absolute -botton-3 w-0 h-0 border-l-[10px] border-r-[10px]  border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+            isOpen && "opacity-100"
+          )}>
+
+          <SubcategoryMenu 
+            category={category}
+            isOpen={isOpen}
+            position={dropdownPosition}
+          />
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
---
+++ src/app/(app)/(home)/search-filter/use-dropdown-position.tsx
@@ -0,0 +1,41 @@
+import  { RefObject } from 'react'
+
+const UseDropdownPosition = (
+  ref: RefObject<HTMLDivElement | null> | RefObject<HTMLDivElement>
+) => {
+
+  const getDropdownPosition = () => {
+    if(!ref.current) return { top: 0, left: 0 };
+
+    const rect = ref.current.getBoundingClientRect();           //  Se obtiene la posición y tamaño del elemento en pantalla con getBoundingClientRect().
+
+    const dropdownWidth = 240;                                  // Ancho del dropdown
+
+    // Intenta colocar el dropdown debajo del elemento
+    let left = rect.left + window.scrollX;                      // Calcula la posición horizontal (left) y vertical (top) tomando en cuenta el scroll de la página  
+    let top = rect.bottom + window.scrollY;                     // para posicionar correctamente el dropdown justo debajo del elemento.
+
+    // Si se sale por la derecha, moverlo hacia la izquierda
+    if (left + dropdownWidth > window.innerWidth) {             // Si el dropdown se sale del lado derecho:
+      left = rect.right + window.scrollX - dropdownWidth        // Ajusta la posición hacia la izquierda para que el dropdown no se desborde.
+      
+      // Si aún así se sale por la izquierda, 
+      // colocarlo fijo cerca del borde derecho
+      if (left < 0) {                                             // Luego, si después de ese cálculo el left termina siendo negativo (es decir, se iría fuera por el lado izquierdo),  
+        left = window.innerWidth - dropdownWidth - 16             // se corrige forzándolo a estar dentro del viewport:  
+      }
+    }
+
+
+    if(left < 0){
+      left = 16;
+    }
+
+    return { top, left }
+
+  }
+
+  return { getDropdownPosition}
+}
+
+export default UseDropdownPosition
