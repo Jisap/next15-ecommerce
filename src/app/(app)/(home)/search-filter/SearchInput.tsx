e59c66d7d6959ff56881d87080b62acc0ee788e6
Jorge Matos <jisapjisap@gmail.com>
2025-04-25 17:05:32 +0200

    Enhance SearchInput Component: Integrate session check to conditionally render a Library button for authenticated users, and import additional icons for improved UI. Update state management with useTRPC and useQuery for session handling.

--- src/app/(app)/(home)/search-filter/SearchInput.tsx
+++ src/app/(app)/(home)/search-filter/SearchInput.tsx
@@ -1,10 +1,13 @@
 "use client"
 
 import { Input } from "@/components/ui/input";
-import { List, ListFilterIcon, SearchIcon } from "lucide-react";
+import { BookmarkCheckIcon, List, ListFilterIcon, SearchIcon } from "lucide-react";
 import CategoriesSidebar from "./categories-sidebar";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/app/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 interface Props {
   disabled?: boolean;
@@ -13,6 +16,8 @@ interface Props {
 export const SearchInput = ({ disabled }: Props) => {
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const trpc = useTRPC();
+  const session = useQuery(trpc.auth.session.queryOptions());
 
   return (
     <div className="flex items-center gap-2 w-full">
@@ -38,6 +43,18 @@ export const SearchInput = ({ disabled }: Props) => {
       >
         <ListFilterIcon className="" />
       </Button>
+
+      {session.data?.user && (
+        <Button 
+          asChild
+          variant="elevated"
+        >
+          <Link href="/library">
+            <BookmarkCheckIcon />
+            Library
+          </Link>
+        </Button>
+      )}
     </div>
   )
 }
