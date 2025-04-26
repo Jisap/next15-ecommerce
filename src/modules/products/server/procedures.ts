b3768a55785f2837e24c62895d83f1fca7a4fe83
Jorge Matos <jisapjisap@gmail.com>
2025-04-26 19:13:08 +0200

    Enhance TypeScript Configuration and Refactor Product Procedures: Add 'noUncheckedIndexedAccess' to tsconfig for stricter type checking. Update comments in product retrieval logic to improve clarity and maintainability, ensuring better understanding of category and subcategory relationships in queries.

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -17,10 +17,10 @@ export const productsRouter = createTRPCRouter({
       const where: Where = {};
 
       if(input.category) {    
-        const categoriesData = await ctx.db.find({             // 1º Se busca la categoría con el slug proporcionado por el usuario
+        const categoriesData = await ctx.db.find({                // 1º Se busca la categoría con el slug proporcionado por el usuario
           collection: "categories",
-          limit: 1,                                            // Se limita a 1 resultado (los slugs son únicos)
-          depth: 1,                                            // Se hace populate de "subcategories"
+          limit: 1,                                               // Se limita a 1 resultado (los slugs son únicos)
+          depth: 1,                                               // Se hace populate de "subcategories"
           pagination:false,
           where: {
             slug: {
@@ -29,7 +29,7 @@ export const productsRouter = createTRPCRouter({
           }
         })
 
-        const formattedData = categoriesData.docs.map((doc) => ({
+        const formattedData = categoriesData.docs.map((doc) => ({  // Se formatea el dato de la categoría encontrada
           ...doc,
           subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
             ...(doc as Category),      // Se hace esto porque depth: 1 no devuelve el tipo correcto
@@ -38,26 +38,26 @@ export const productsRouter = createTRPCRouter({
         }))
 
         const subcategoriesSlugs = [];
-        const parentCategory = formattedData[0];               // Se obtiene la categoría padre
+        const parentCategory = formattedData[0];                 // Se obtiene la categoría padre
 
-        if(parentCategory){                                    // Si la categoría padre existe
-          subcategoriesSlugs.push(                             // Recolecta los slugs de las subcategorías DIRECTAS.
+        if(parentCategory){                                      // Si la categoría padre existe
+          subcategoriesSlugs.push(                               // Recolecta los slugs de las subcategorías DIRECTAS.
             ...parentCategory.subcategories.map((subcategory) => subcategory.slug) // mapea las subcategorías (obtenidas por depth 1) y extrae su slugs
           )
-        }
 
-        where["category.slug"] = {                             // Condición de busqueda a la consulta: Busca productos cuyo slug de categoría asociada cumpla con esta condición 
-          in: [                                                // estar en la lista
-            parentCategory.slug,                               // de las categorías padre
-            ...subcategoriesSlugs                              // o de las subcategorías directas
-          ]
-        }                 
+          where["category.slug"] = {                             // Se establece la condición de busqueda de productos basada en los slugs de las subcategorias: Busca productos cuyo slug de categoría asociada cumpla con esta condición 
+            in: [                                                // estar en la lista
+              parentCategory.slug,                               // de las categorías padre
+              ...subcategoriesSlugs                              // o de las subcategorías directas
+            ]
+          }                 
+        } 
       }
       
-      const data = await ctx.db.find({                         // 2º consulta a Payload
-        collection: "products",                                // Se busca en la colección "products"
-        depth: 1,                                              // Se hace populate de "category" & "image"
-        where                                                  // Filtro: el slug de la categoría del producto debe coincidir con el de la categoría principal o cualquiera de sus subcategorías
+      const data = await ctx.db.find({                           // 2º consulta a Payload
+        collection: "products",                                  // Se busca en la colección "products"
+        depth: 1,                                                // Se hace populate de "category" & "image"
+        where                                                    // Filtro: el slug de la categoría del producto debe coincidir con el de la categoría principal o cualquiera de sus subcategorías
       })
   
       return data;
