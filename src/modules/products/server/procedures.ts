2b9928463f89ec5ffc35299108300a7407452473
Jorge Matos <jisapjisap@gmail.com>
2025-04-26 18:30:51 +0200

    Enhance Product Query Logic: Refactor TRPC procedures to include category and subcategory filtering for product retrieval. Implement logic to fetch parent categories and their direct subcategories, improving data accuracy and user experience in product listings.

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,5 +1,6 @@
 
 import { baseProcedure, createTRPCRouter } from "@/app/trpc/init";
+import { Category } from "@/payload-types";
 import type { Where } from "payload";
 import { z } from "zod";
 
@@ -16,19 +17,52 @@ export const productsRouter = createTRPCRouter({
       const where: Where = {};
 
       if(input.category) {    
-          where["category.slug"] = {               // Se agrega una condición de busqueda a la consulta
-            equals: input.category,                // category.slug === input.category
+        const categoriesData = await ctx.db.find({             // 1º Se busca la categoría con el slug proporcionado por el usuario
+          collection: "categories",
+          limit: 1,                                            // Se limita a 1 resultado (los slugs son únicos)
+          depth: 1,                                            // Se hace populate de "subcategories"
+          pagination:false,
+          where: {
+            slug: {
+              equals: input.category
+            }
           }
+        })
+
+        const formattedData = categoriesData.docs.map((doc) => ({
+          ...doc,
+          subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+            ...(doc as Category),      // Se hace esto porque depth: 1 no devuelve el tipo correcto
+            subcategories: undefined,  // No se hace populate de las subcategories aninadas debido a depth: 1 
+          }))
+        }))
+
+        const subcategoriesSlugs = [];
+        const parentCategory = formattedData[0];               // Se obtiene la categoría padre
+
+        if(parentCategory){                                    // Si la categoría padre existe
+          subcategoriesSlugs.push(                             // Recolecta los slugs de las subcategorías DIRECTAS.
+            ...parentCategory.subcategories.map((subcategory) => subcategory.slug) // mapea las subcategorías (obtenidas por depth 1) y extrae su slugs
+          )
+        }
+
+        where["category.slug"] = {                             // Condición de busqueda a la consulta: Busca productos cuyo slug de categoría asociada cumpla con esta condición 
+          in: [                                                // estar en la lista
+            parentCategory.slug,                               // de las categorías padre
+            ...subcategoriesSlugs                              // o de las subcategorías directas
+          ]
+        }                 
       }
+      
+      const data = await ctx.db.find({                         // 2º consulta a Payload
+        collection: "products",                                // Se busca en la colección "products"
+        depth: 1,                                              // Se hace populate de "category" & "image"
+        where                                                  // Filtro: el slug de la categoría del producto debe coincidir con el de la categoría principal o cualquiera de sus subcategorías
+      })
+  
+      return data;
+    
 
-    const data = await ctx.db.find({                // La consulta es a Payload
-      collection: "products",                       // Se busca en la colección "products"
-      depth: 1,                                     // Se hace populate de "category" & "image"
-      where                                         // Se aplica la condición de busqueda: category.slug === input.category
-    })
 
-    //await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulación de una tarea de procesamiento de datos
-
-    return data;
   })
 })
