f60e28229865776e7ef4fafed25c18a33634fd65
Jorge Matos <jisapjisap@gmail.com>
2025-04-26 15:06:58 +0200

    Add Products Support: Introduce Product and ProductsSelect interfaces in payload-types, update Config to include products, and modify payload.config to register Products collection, enhancing product management capabilities.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -15,6 +15,7 @@ import sharp from 'sharp'
 import  { Users }  from './collections/Users'
 import  { Media } from './collections/Media'
 import  { Categories } from './collections/Categories'
+import { Products } from './collections/Products'
 
 
 const filename = fileURLToPath(import.meta.url)
@@ -27,7 +28,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, Categories],
+  collections: [Users, Media, Categories, Products],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
