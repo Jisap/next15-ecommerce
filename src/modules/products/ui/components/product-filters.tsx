d43cdab1a6647e34f1c06a7ad4d55db2a618c3a7
Jorge Matos <jisapjisap@gmail.com>
2025-04-27 15:48:11 +0200

    Add Product Filters Component: Implement a collapsible ProductFilter component for better organization of filter options in the product listing. Include a clear button for resetting filters and enhance the overall user experience with improved UI elements.

--- src/modules/products/ui/components/product-filters.tsx
+++ src/modules/products/ui/components/product-filters.tsx
@@ -1,12 +1,57 @@
+"use client"
 
+import { useState } from 'react';
+import { Product } from '../../../../payload-types';
+import { ChevronDownIcon, ChevronUpIcon } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
+interface ProductFiltersProps {
+  title: string;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const ProductFilter = ({ title, className, children }: ProductFiltersProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const Icon = isOpen ? ChevronUpIcon : ChevronDownIcon;
+
+  return (
+    <div className={cn(
+      "p-4 border-b flex flex-col gap-2",
+      className
+    )}>
+      <div
+        onClick={() => setIsOpen((current) => !current)}
+        className='flex items-center justify-between cursor-pointer'
+      >
+        <p className='font-medium'>{title}</p>
+        <Icon className='size-5' />
+      </div>
+
+      {isOpen && children}
+    </div>
+  )
+}
 
 export const ProductFilters = () => {
   return (
     <div className="border rounded-md bg-white">
+      {/* Cabecera de filtros */}
       <div className="p-4 border-b flex items-center justify-between">
         <p className="font-medium">Filters</p>
+        <button 
+          className="underline"
+          onClick={() => {}}
+          type="button"  
+        >
+          Clear
+        </button>
       </div>
+
+      {/* Filtros de productos */}
+      <ProductFilter title="Price" className='border-b-0'>
+        <p>Price filter</p>
+      </ProductFilter>
     </div>
   )
 }
