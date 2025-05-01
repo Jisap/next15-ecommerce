6c89035c32381cf347ba1a8ca112893e3a386e1c
Jorge Matos <jisapjisap@gmail.com>
2025-05-01 16:36:06 +0200

    Refactor Home Page Component: Update the Home page to utilize async data fetching with search parameters, integrate product filters, and implement prefetching of products using the new ProductListView component for improved performance and user experience.

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,21 +1,32 @@
-"use client"
+import type { SearchParams } from "nuqs/server";
+import { getQueryClient, trpc } from "@/app/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { loadProductFilters } from "@/modules/products/search-params";
+import { ProductListView } from "@/modules/products/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constants";
 
 
 
-import { useTRPC } from "@/app/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 
+interface Props {
+  searchParams: Promise<SearchParams>          // Se reciben los searchParams de la consulta de la URL [minPrice, maxPrice]
+}
 
-export default function Home() {
+const Page = async ({ searchParams }: Props) => {
 
-  const trpc = useTRPC();
-  //const categories = useQuery(trpc.categories.getMany.queryOptions()); // petición de datos en el router categories que llama al procedimiento getMany
-  const { data } = useQuery(trpc.auth.session.queryOptions());
+  const filters = await loadProductFilters(searchParams);
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchInfiniteQuery(trpc.products.getMany.infiniteQueryOptions({ // Prefetch de productos con populate de category y filtro aplicados en url
+    ...filters,
+    limit: DEFAULT_LIMIT
+  }));
 
   return (
-    <div className="p-4">
-     {/* {JSON.stringify(categories.data, null, 2)} */}
-     {JSON.stringify(data?.user, null, 2)}
-    </div>
-  );
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductListView />
+    </HydrationBoundary>
+  )
 }
+
+export default Page
