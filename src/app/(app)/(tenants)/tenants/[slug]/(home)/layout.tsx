540975692bd2cb53a14f2a7000ad8f20ab44a1c8
Jorge Matos <jisapjisap@gmail.com>
2025-05-01 17:45:09 +0200

    Remove Tenant Page Component: Delete the tenant-specific page component that handled product listing and filtering by tenant slug. This cleanup is part of the ongoing refactor to streamline the codebase and improve maintainability.

---
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/modules/tenants/ui/components/navbar";
+
+
+
+interface LayoutProps {
+  children: React.ReactNode;
+  params: Promise<{slug: string}>;
+
+}
+
+const Layout = async({ children }: LayoutProps) => {
+
+
+  return (
+    <div className='min-h-screen bg-[#F4F4F0] flex flex-col'> 
+      <Navbar />
+      {children}
+    </div>
+  )
+}
+
+export default Layout
