a88832d467f2ea3a1068767c3fdc70e9e6ea9c96
Jorge Matos <jisapjisap@gmail.com>
2025-05-02 16:05:41 +0200

    Add Footer Component to Tenant Layout: Integrate the Footer component into the tenant layout for improved user experience and consistent design across pages.

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/modules/tenants/ui/components/footer";
 import { Navbar } from "@/modules/tenants/ui/components/navbar";
 
 
@@ -16,6 +17,7 @@ const Layout = async({ children }: LayoutProps) => {
     <div className='min-h-screen bg-[#F4F4F0] flex flex-col'> 
       <Navbar />
       {children}
+      <Footer />
     </div>
   )
 }
