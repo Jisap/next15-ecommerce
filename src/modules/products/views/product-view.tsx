58111761d0c825f2e60828c928aaac4fa2c757b5
Jorge Matos <jisapjisap@gmail.com>
2025-05-03 17:30:26 +0200

    Enhance Product View with Additional Details: Update ProductView component to include product description, mobile rating display, and an "Add to cart" button. Improve layout structure for better user experience and accessibility of product information.

--- src/modules/products/views/product-view.tsx
+++ src/modules/products/views/product-view.tsx
@@ -2,8 +2,10 @@
 
 import { useTRPC } from '@/app/trpc/client';
 import { StarRating } from '@/components/star-rating';
+import { Button } from '@/components/ui/button';
 import { formatCurrency, generateTenantURL } from '@/lib/utils';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { LinkIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -21,6 +23,7 @@ export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
   return (
     <div className='px-4 lg:px-12 py-10'>
       <div className='border rounded-sm bg-white overflow-hidden'>
+        {/* Imagen del producto */}
         <div className='relative aspect-[3.9] border-b'>
           <Image
             src={data.image?.url || "/placeholder.png"}
@@ -30,14 +33,18 @@ export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
           />
         </div>
 
+        {/* Información del producto */}
         <div className='grid grid-cols-1 lg:grid-cols-6'>
+          {/* Left col-span-4*/}
           <div className='col-span-4'>
+            {/* Name */}
             <div className='p-6'>
               <h1 className='text-4xl font-medium'>
                 {data.name}
               </h1>
             </div>
 
+            {/* Price - tenant - rating */}
             <div className='border-y flex'>
               <div className='px-6 py-4 flex items-center justify-center border-r'>
                 <div className='relative px-2 py-1 border bg-pink-400 w-fit'>
@@ -48,12 +55,12 @@ export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
               </div>
 
               <div className='px-6 py-4 flex items-center justify-center lg:border-r'>
-                <Link 
-                  href={generateTenantURL(tenantSlug)} 
+                <Link
+                  href={generateTenantURL(tenantSlug)}
                   className='flex items-center gap-2'
                 >
                   {data.tenant.image?.url && (
-                    <Image 
+                    <Image
                       src={data.tenant.image.url}
                       alt={data.tenant.name}
                       width={20}
@@ -69,11 +76,67 @@ export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
 
               <div className='hidden lg:flex px-6 py-4 items-center justify-center'>
                 <div className='flex items-center gap-1'>
-                  <StarRating 
+                  <StarRating
                     rating={3}
                     iconClassName='size-4'
                   />
                 </div>
+              </div>
+            </div>
+            
+            {/* Mobile rating */}
+            <div className='block lg:hidden px-6 py-4 items-center justify-center border-b'>
+              <div className='flex items-center gap-1'>
+                <StarRating
+                  rating={3}
+                  iconClassName='size-4'
+                />
+                <p className='text-xs font-medium'>
+                  {5} ratings
+                </p>
+              </div>
+            </div>
+
+            {/* description */}
+            <div className='p-6'>
+              {data.description ? (
+                <p>{data.description}</p>
+              ) : (
+                <p className='font-medium text-muted-foreground italic'>
+                  No description provided
+                </p>
+              )}
+            </div>
+          </div>
+
+          {/* Right col-span-2 */}
+          <div className='col-span-2'>
+            <div className='border-t lg:border-t-0 lg:border-l h-full'>
+              <div className='flex flex-col gap-4 p-6 border-b'>
+                <div className='flex flex-row items-center gap-2'>
+                  <Button
+                    variant="elevated"
+                    className='flex-1 bg-pink-400'
+                  >
+                    Add to cart
+                  </Button>
+
+                  <Button
+                    className='size-12'
+                    variant="elevated"
+                    onClick={() => { }}
+                    disabled={false}
+                  >
+                    <LinkIcon />
+                  </Button>
+                </div>
+
+                <p className='text-center font-medium'>
+                  {data.refundPolicy === "no-refund"
+                    ? "No refund"
+                    : `${data.refundPolicy} money back guarantee`
+                  }
+                </p>
               </div>
             </div>
           </div>
