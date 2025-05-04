8209e14fc3f0849119a247405970c9c0af409944
Jorge Matos <jisapjisap@gmail.com>
2025-05-04 11:42:14 +0200

    Improve CartButton Feedback in ProductView: Update the CartButton component to provide clearer loading feedback and maintain consistent styling during asynchronous operations. This enhancement ensures users are informed of the button's state, improving overall user experience.

---
+++ src/modules/checkout/ui/components/checkout-button.tsx
@@ -0,0 +1,33 @@
+import { cn, generateTenantURL } from "@/lib/utils";
+import { useCart } from "../../hooks/use-cart";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ShoppingCartIcon } from "lucide-react";
+
+
+
+
+interface CheckoutButtonProps {
+  className?: string;
+  hideIfEmpty?: boolean;
+  tenantSlug: string;
+}
+
+export const CheckoutButton = ({ className, hideIfEmpty, tenantSlug }: CheckoutButtonProps) => {
+
+  const { totalItems } = useCart(tenantSlug);
+  if(hideIfEmpty && totalItems === 0) return null;
+
+  return (
+    <Button
+      asChild
+      variant="elevated"
+      className={cn("bg-white", className)}
+    >
+      {/* /tenants/${tenantSlug}/checkout */}
+      <Link href={`${generateTenantURL(tenantSlug)}/checkout`}>
+        <ShoppingCartIcon /> {totalItems > 0 ? totalItems : ""}
+      </Link>
+    </Button>
+  )
+}
