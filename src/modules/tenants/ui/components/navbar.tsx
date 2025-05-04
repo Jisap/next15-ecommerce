0c9669f6f2669e88b900630205a9ba12427a481c
Jorge Matos <jisapjisap@gmail.com>
2025-05-04 11:51:13 +0200

    Add CheckoutButton to Navbar: Integrate dynamic CheckoutButton component into the Navbar for improved cart functionality. This change enhances user experience by providing immediate feedback during loading states and ensures proper client-side rendering.

--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -2,11 +2,29 @@
 
 
 import { useTRPC } from '@/app/trpc/client';
+import { Button } from '@/components/ui/button';
 import { generateTenantURL } from '@/lib/utils';
+//import { CheckoutButton } from '@/modules/checkout/ui/components/checkout-button';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { ShoppingCart } from 'lucide-react';
+import dynamic from 'next/dynamic';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
+const CheckoutButton = dynamic(() => import('@/modules/checkout/ui/components/checkout-button').then(  // Solo después de que la carga inicial y la hidratación principal han ocurrido, el CartButton se monta y renderiza por primera vez, ya directamente en el entorno del cliente.
+  (mod) => mod.CheckoutButton                                                                       // En este punto, useCart puede acceder a localStorage sin problemas y el botón se renderiza con el estado correcto desde el principio (en el cliente),
+), {
+  ssr: false,                                                                                       // ssr: false no renderiza el componente en el server-side
+  loading: () => (
+    <Button 
+      disabled 
+      className='bg-white'
+    >
+      <ShoppingCart className='text-black' />
+    </Button>),
+}
+); 
 
 interface Props {
   slug: string;
@@ -32,6 +50,10 @@ export const Navbar = ({ slug }: Props ) => {
           )}
           <p className='text-xl'>{data.name}</p>
         </Link>
+
+        <CheckoutButton 
+          tenantSlug={slug}
+        />
       </div>
     </nav>
   );
