0ceebe777f8b91785a98f45ab361f803fdeb9d2a
Jorge Matos <jisapjisap@gmail.com>
2025-05-05 14:38:45 +0200

    Implement Error Handling in CheckoutView: Add error handling logic to clear the cart and notify users when invalid products are detected. This enhancement improves user experience by ensuring the cart remains valid and providing feedback through toast notifications.

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -4,6 +4,9 @@ import { useTRPC } from '@/app/trpc/client';
 import { trpc } from '../../../../app/trpc/server';
 import { useQuery } from '@tanstack/react-query';
 import { useCart } from '../../hooks/use-cart';
+import { useEffect } from 'react';
+import { toast } from 'sonner';
+
 
 interface CheckoutViewProps {
   tenantSlug: string;
@@ -11,14 +14,22 @@ interface CheckoutViewProps {
 
 export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
 
-  const { productIds } = useCart(tenantSlug)
+  const { productIds, clearAllCarts } = useCart(tenantSlug)
   
   const trpc = useTRPC();
-  const { data } = useQuery(trpc.checkout.getProducts.queryOptions({ 
+  const { data, error } = useQuery(trpc.checkout.getProducts.queryOptions({ 
     ids: productIds
   }));
+
+  useEffect(() => {
+    if( !error ) return;
+    if(error.data?.code === 'NOT_FOUND') {
+      clearAllCarts();
+      toast.warning("Invalid products found, cart cleared")
+    }
+  },[error, clearAllCarts])
   
-  return (
+   return (
     <div>
       {JSON.stringify(data, null, 2)}
     </div>
