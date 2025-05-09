8f7a2d8f402fca1542884d7f6bf6d3f102f56c0e
Jorge Matos <jisapjisap@gmail.com>
2025-05-09 17:13:00 +0200

    Add Update Review Procedure to Reviews Router: Implement a new protected procedure for updating reviews, including input validation and error handling for non-existent reviews and permission checks, enhancing the reviews functionality in the API.

--- src/modules/reviews/server/procedures.ts
+++ src/modules/reviews/server/procedures.ts
@@ -112,5 +112,47 @@ export const reviewsRouter = createTRPCRouter({           // reviewsRouter defin
 
       return review;
 
-  })
+  }),
+
+  update: protectedProcedure                                                       // Endpoint updatete para actualizar una review
+    .input(
+      z.object({
+        reviewId: z.string(),
+        rating: z.number().min(1, { message: "Rating is required" }).max(5),
+        description: z.string().min(1, { message: "Description is required" }),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const existingReview = await ctx.db.findByID({                                      // Se busca la review en base al id de la review
+        collection: 'reviews',
+        depth: 0, // existingReview.user -> user ID
+        id: input.reviewId,
+      });
+
+      if(!existingReview){
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Review not found",
+        })
+      }
+
+      if(existingReview.user !== ctx.session.user.id){
+        throw new TRPCError({
+          code: "FORBIDDEN",
+          message: "You are not allowed to update this review",
+        })
+      }
+
+      const updatedReview = await ctx.db.update({                                         // Si hay una review existente, se actualiza
+        collection: "reviews",
+        id: input.reviewId,
+        data: {
+          rating: input.rating,
+          description: input.description,
+        }
+      })
+
+      return updatedReview
+
+    })
 })
