7966c8ccd42d6bb11d6bc0d7f22615b5d682aaaa
Jorge Matos <jisapjisap@gmail.com>
2025-05-10 15:25:34 +0200

    Implement Review Form Component with Validation: Add a new ReviewForm component that utilizes react-hook-form and Zod for input validation, allowing users to submit and preview reviews with a structured form layout.

--- src/modules/library/ui/components/review-form.tsx
+++ src/modules/library/ui/components/review-form.tsx
@@ -1,14 +1,90 @@
 import { ReviewsGetOneOutput } from "@/modules/reviews/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
+
+import { useTRPC } from "@/app/trpc/client";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+//import { StarPicker } from "@/components/star-picker"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+ 
 interface Props {
   productId: string;
   initialData?: ReviewsGetOneOutput;
 }
 
+const formScheam = z.object({
+  rating: z.number().min(1, {message: "Rating is required"}).max(5),
+  description: z.string().min(1, {message: "Description is required"}),
+});
+
+
+
+
 export const ReviewForm = ({ productId, initialData}: Props) => {
+
+  const [isPreview, setIsPreview] = useState(!!initialData); // Si el producto tiene reviews previamente, se muestra el botón de previsualización.
+
+  const form = useForm<z.infer<typeof formScheam>>({
+    resolver: zodResolver(formScheam),
+    defaultValues: {
+      rating: initialData?.rating ?? 0,
+      description: initialData?.description ?? "",
+    }
+  });
+
+  const onSubmit = (data: z.infer<typeof formScheam>) => {
+    console.log(data);
+  }
+
   return (
-    <div>
-      ReviewForm
-    </div>
+    <Form {...form}>
+      <form
+        className="flex flex-col gap-y-4"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <p className="font-medium">
+          {isPreview ? "Your rating" : "Liked it? Give it a rating"}
+        </p>
+
+        <FormField 
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Textarea 
+                  placeholder="Want to leave a written review ?"
+                  disabled={isPreview}
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        {!isPreview && (
+          <Button
+            variant="elevated"
+            disabled={false}
+            type="submit"
+            size="lg"
+            className="bg-black text-white hover:bg-pink-400 hover:text-primary w-fit"
+          >
+            {initialData ? "Update review" : "Post review" }
+          </Button>
+        )}
+      </form>
+    </Form>
   )
 }
