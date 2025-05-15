ee54b854eecc164920b5253650c2f303b7ed5f56
Jorge Matos <jisapjisap@gmail.com>
2025-05-15 19:37:40 +0200

    comments

--- src/app/(app)/(library)/library/page.tsx
+++ src/app/(app)/(library)/library/page.tsx
@@ -1,7 +1,7 @@
 import { getQueryClient, trpc } from '@/app/trpc/server';
 import { DEFAULT_LIMIT } from '@/constants';
 import LibraryView from '@/modules/library/ui/views/library-view'
-import { ProductViewSkeleton } from '@/modules/library/ui/views/product-view';
+import { ProductViewSkeleton } from '@/modules/library/ui/views/product-view'; // No confundir con el del producto
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import { Suspense } from 'react';
 
